0733d7f92e70a2c07a2543cd424a36f924f07d1b
zeronosu77108 <taiga77108@gmail.com>
2024-08-19 20:58:17 +0900

    Generate unique filenames for Rust source and input files
    Previously, the Rust source and input files were overwritten each time, which could lead to conflicts. Now, unique filenames are generated using `SecureRandom.hex` for both source code and input files. Unnecessary files are also deleted after execution to maintain cleanliness.

--- src/main.rb
+++ src/main.rb
@@ -1,5 +1,6 @@
 require 'grape'
 require 'open3'
+require 'securerandom'
 
 class Main < Grape::API
   format :json
@@ -10,23 +11,30 @@ class Main < Grape::API
     source_code = request_payload['source_code']
     input = request_payload['input']
 
-    # main.rs にソースコードを書き込む
-    File.open('./rust/src/main.rs', 'w') do |file|
+    filename = SecureRandom.hex(10) + '.rs'
+    # ソースコードを書き込む
+    File.open("./rust/src/#{filename}", 'w') do |file|
       file.write(source_code)
     end
 
-    # input.txt に入力を書き込む
-    File.open('./rust/input.txt', 'w') do |file|
+    input_filename = SecureRandom.hex(10) + '.txt'
+    # 入力を書き込む
+    File.open("./rust/#{input_filename}", 'w') do |file|
       file.write(input)
     end
 
     command = "cd ./rust;"
-    command += "/root/.cargo/bin/cargo run --release < input.txt"
+    command += "/root/.cargo/bin/cargo run --release < #{input_filename}"
 
     # 実行時間を計測する
     start_time = Time.now
     result, err, status = Open3.capture3(command)
     end_time = Time.now
+
+
+    # 不要ファイルの削除
+    File.delete("./rust/src/#{filename}")
+    File.delete("./rust/#{input_filename}")
 
     if status.exitstatus != 0
       return { error: err.to_s, result: result }
